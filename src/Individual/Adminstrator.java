3f252dedad8c7130337e691e4f5acc1c11cac23b
Ibrahim Owaidat <ibrahim.owaidat@outlook.com>
2018-06-09 18:04:31 +0300

    Update Adminstrator.java

--- src/Individual/Adminstrator.java
+++ src/Individual/Adminstrator.java
@@ -1,7 +1,13 @@
 package Individual;
 
-public class Adminstrator {
+import java.io.Serializable;
 
+public class Adminstrator implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String Username;
 	private String Password;
 
